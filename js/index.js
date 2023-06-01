98e07fdbe9f08c19b31bc776896ea3d469de97ed
lance <3131059028@qq.com>
2023-06-01 12:10:30 +0800

    第一部分完成

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(".list li").click(function(){
+            $(this).addClass("active").siblings().removeClass("active")
+            var  index = $(this).index()
+            $(".box ul").eq(index).addClass("active").siblings().removeClass("active")
+        })
+let liItem = $(".box ul li")
+liItem.click(function(){
+    // console.log($(this).text())
+    setTimeout(delClass,1000);
+    $("#lance").addClass($(this).text())
+    $(this).addClass("active").siblings().removeClass("active")
+})
+function delClass(){
+    $("#lance").removeClass()
+}
+$(".explain div").click(function(){
+    $(this).addClass("item").siblings().removeClass("item")
+    var index = $(this).index()
+    $(".illustrate div").eq(index).addClass("item").siblings().removeClass("item")
+})
+$("#self").click(function(){
+    location.href = "../css/lance.css"
+})
+$("#animate").click(function(){
+    location.href = "../css/animate.css"
+})
+$("#animate2").click(function(){
+    location.href = "https://animate.style/"
+})
